d823fc6049a84bf87ef1f5a786b7645862c78d06
marcus quigley <marcusquigley@gmail.com>
2020-12-19 14:00:04 -0500

    WIP Manage State Redux

---
+++ truck-frontend/src/reducers/productreducer.js
@@ -0,0 +1,3 @@
+function productListReducer(state = {}, )
+//goto https://www.youtube.com/watch?v=Fy9SdZLBTOo&t=506s
+//chaper 11
---
+++ truck-frontend/src/store.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux';
+const initialState = {};
+const reducer=null;
+
+const store = createStore(reducer, initialState);
